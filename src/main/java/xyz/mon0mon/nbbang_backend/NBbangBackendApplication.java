784c070858a93812d10ea545ff3a0d83ef7abd1f
ARA <mon0mon@naver.com>
2023-12-23 14:04:56 +0900

    🚀 init : 새 프로젝트 시작!

---
+++ src/main/java/xyz/mon0mon/nbbang_backend/NBbangBackendApplication.java
@@ -0,0 +1,13 @@
+package xyz.mon0mon.nbbang_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NBbangBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NBbangBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/mon0mon/nbbang_backend/NBbangBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.mon0mon.nbbang_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NBbangBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
